0fe46e32abc5690c34e37b576af989a8d7127e8a
liyin <uscliyin@gmail.com>
2023-04-28 14:22:27 -0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,5 +32,19 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services=scope.ServiceProvider;
+var context=services.GetRequiredService<StoreContext>();
+var logger=services.GetRequiredService<ILogger<Program>>();
+
+try{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception){
+    logger.LogError("An error occured during migration");
+}
+
 
 app.Run();
